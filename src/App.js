3cdc3c08d8ef780ca400abe3fae979d58ac0d370
Guy Kibby <GuyKibby@gmail.com>
2022-03-27 20:20:55 +1300

    experimental

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
-import Timer from "./timer.js";
-import start from "./timer.js";
+
+let state = "PAUSE";
+let prevState = "PAUSE";
+
+let prevTime = 0;
+let startTime = Date.now();
+let newTime = 0;
+
+const calcTime = () => {
+  if (state === "WORK") {
+    newTime = prevTime + (Date.now() - startTime) / 1000;
+    // console.log("working");
+  } else if (state === "PLAY") {
+    newTime = prevTime - (Date.now() - startTime) / 1000;
+    // console.log("playing");
+  } else {
+    // console.log("paused");
+  }
+  // console.log("newTime:" + Math.floor(newTime));
+  // return newTime;
+};
+
+let x = setInterval(calcTime, 1000);
+
+const switchstate = () => {
+  prevTime = newTime;
+  startTime = Date.now();
+
+  if (state === "WORK") {
+    state = "PLAY";
+  } else {
+    state = "WORK";
+  }
+  return;
+};
+
+const pauseTime = () => {
+  if (state !== "PAUSE") {
+    prevState = state;
+    state = "PAUSE";
+  } else {
+    prevTime = newTime;
+    startTime = Date.now();
+    state = prevState;
+  }
+};
 
 function App() {
+  const [timerTime, whatTime] = useState(5);
+  console.log(timerTime);
+
+  const checkTime = () => {
+    calcTime();
+
+    if (timerTime !== Math.floor(newTime)) {
+      whatTime(Math.floor(newTime));
+    }
+    // console.log("value:" + timerTime);
+  };
+
+  setInterval(checkTime, 1000);
+
   return (
     <div className="App">
       <header className="App-header">
-        <Timer />
-
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+        <div>{timerTime}</div>
+        <button onClick={switchstate}>WORK/PLAY</button>
+        <button onClick={pauseTime}>PAUSE</button>
       </header>
     </div>
   );
